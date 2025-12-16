4e7f866a434a87feb9d7585e66598c1de5ba3040
Guilherme <143561085+Guilhermeb166@users.noreply.github.com>
2025-12-16 15:39:42 -0300

    removido lógica do next-auth no loading

--- src/components/AuthLoader.jsx
+++ src/components/AuthLoader.jsx
@@ -1,24 +1,36 @@
 "use client"
-import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "@/backend/firebase"
 import { useExpenses } from "@/context/AppContext"
 import { MdAutorenew } from "react-icons/md"
 
-export default function AuthLoader({children}) {
-
-    const {status} = useSession()
+export default function AuthLoader({ children }) {
+    const [authLoading, setAuthLoading] = useState(true)
     const { expenses } = useExpenses()
 
-    const isLoading =
-    status === "loading" ||               
-    (status === "authenticated" && expenses === null) 
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, () => {
+            setAuthLoading(false)
+        })
 
-  if (isLoading) {
-    return (
-      <div className="w-full h-screen flex flex-col items-center justify-center gap-4 bg-white">
-        <MdAutorenew size={55} className="animate-spin text-emerald-600" />
-      </div>
-    )
-  }
+        return () => unsub()
+    }, [])
+
+    const isLoading =
+        authLoading ||
+        (auth.currentUser && expenses === null)
+
+    if (isLoading) {
+        return (
+            <div className="w-full h-screen flex flex-col items-center justify-center gap-4 bg-white">
+                <MdAutorenew
+                    size={55}
+                    className="animate-spin text-emerald-600"
+                />
+            </div>
+        )
+    }
 
     return children
 }
